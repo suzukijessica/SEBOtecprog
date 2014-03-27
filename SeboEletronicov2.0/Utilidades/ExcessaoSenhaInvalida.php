66cb6cff3ec2db20b00481262875ef639bba7be1
tainarareis <tainarareis@192.168.1.3>
2014-03-27 10:58:32 -0300

    Applying  header in class ExcessaoSenhaInvalida.php

--- SeboEletronicov2.0/Utilidades/ExcessaoSenhaInvalida.php
+++ SeboEletronicov2.0/Utilidades/ExcessaoSenhaInvalida.php
@@ -1,5 +1,9 @@
 <?php
-
+/*
+ File name: ExcessaoEmailInvalido.php
+ File description: establishes exception when an invalid password is inserted.
+ Authors: Caique Pereira, Jessica Suzuki, João Gabriel, Macário Soares, Victor Cunha.
+*/ 
 class ExcessaoSenhaInvalida extends Exception {
     
     function __construct($mensagem) {
