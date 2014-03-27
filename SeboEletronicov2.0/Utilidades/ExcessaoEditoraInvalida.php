66e9f644e5186ae8878ed0176b1a2ee5b7a59e92
tainarareis <tainarareis@192.168.1.3>
2014-03-27 10:49:34 -0300

    Applying  header in class ExcessaoEditoraInvalida.php

--- SeboEletronicov2.0/Utilidades/ExcessaoEditoraInvalida.php
+++ SeboEletronicov2.0/Utilidades/ExcessaoEditoraInvalida.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ File name: ExcessaoEditora.php
+ File description: establishes conection to the database.
+ Authors: Caique Pereira, Jessica Suzuki, João Gabriel, Macário Soares, Victor Cunha
+*/ 
 
 class ExcessaoEditoraInvalida extends Exception{
     function __construct($mensagem) {
