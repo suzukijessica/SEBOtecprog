500b00b194df05c0fdfb0a4af8e83da1a3de9aab
Yeltsin <Yeltsin@Yeltsin-PC>
2014-03-27 12:48:53 -0300

    Applying comments and header in class LivroDao

--- SeboEletronicov2.0/Dao/LivroDao.php
+++ SeboEletronicov2.0/Dao/LivroDao.php
@@ -1,11 +1,17 @@
 <?php
+/*
+ File name: LivroDao.php
+ File description: insert, select, update, delete Livros.
+ Authors: Caique Pereira, Jessica Suzuki, João Gabriel, Macário Soares, Victor Cunha.
+*/ 
 
 include "../Utilidades/ConexaoComBanco.php";
 
 class LivroDao {
     
     public function salvaLivro($livro, $id_dono){
-
+	//Inserts Livro in Database
+	
         $sql = "INSERT INTO livro (id_dono, titulo_livro, editora, autor, edicao, genero, estado_conserv, descricao_livro, venda, troca)
             VALUES ('".$id_dono."','".$livro->getTitulo()."','".$livro->getEditora()."','".$livro->getAutor()."',
                 '".$livro->getEdicao()."','".$livro->getGenero()."','".$livro->getEstado()."','".$livro->getDescricao()."','".$livro->getVenda()."',
@@ -15,7 +21,8 @@ class LivroDao {
     }
 
     public function pesquisaLivro($titulo, $estadoNovo, $estadoUsado, $disponibilidadeVenda, $disponibilidadeTroca){
-
+	//Selects Livro in Database
+	
         if(empty($disponibilidadeTroca) && !empty($disponibilidadeVenda)){
             if(empty($estadoNovo) && !empty($estadoUsado)){
                 $sql = "SELECT * FROM livro WHERE titulo_livro = '".$titulo."' AND estado_conserv = '".$estadoUsado."' 
@@ -54,12 +61,15 @@ class LivroDao {
     }
 
     public function deletaLivro($id){
+    //Delete Book of Database
+    	
         $sql = "DELETE FROM livro WHERE id_livro = '".$id."'";
         $deletou = mysql_query($sql);
         return $deletou;
     }
     
     public function alteraLivro($livro, $id_dono, $id_usuario){
+    //Update book parameters in Database	
 
         $sql = "UPDATE livro SET id_dono = '".$id_usuario."', titulo_livro = '".$livro->getTitulo()."', editora = '".$livro->getEditora()."', 
             autor = '".$livro->getAutor()."', edicao = '".$livro->getEdicao()."', genero = '".$livro->getGenero()."', estado_conserv = '".$livro->getEstado()."', 
