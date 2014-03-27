e042dc78ef98f4af9c993b070793306c79e386fa
tainarareis <tainarareis@192.168.1.3>
2014-03-27 10:55:59 -0300

    Applying  header in class ExcessaoEmailInvalido.php

--- SeboEletronicov2.0/Utilidades/ExcessaoEmailInvalido.php
+++ SeboEletronicov2.0/Utilidades/ExcessaoEmailInvalido.php
@@ -1,5 +1,9 @@
 <?php
-
+/*
+ File name: ExcessaoEmailInvalido.php
+ File description: establishes exception when an invalid e-mail is inserted.
+ Authors: Caique Pereira, Jessica Suzuki, João Gabriel, Macário Soares, Victor Cunha
+*/ 
 class ExcessaoEmailInvalido extends Exception{
 	function  __construct($mensagem){
 		parent::__construct($mensagem);
