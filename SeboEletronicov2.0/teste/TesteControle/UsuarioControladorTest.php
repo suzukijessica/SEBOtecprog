265d9e066b8abd04313af24fd6549e30c3b604ab
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2014-03-27 23:34:37 -0300

    applying header in class UsuarioControladorTest.php

--- SeboEletronicov2.0/teste/TesteControle/UsuarioControladorTest.php
+++ SeboEletronicov2.0/teste/TesteControle/UsuarioControladorTest.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ File name: UsuarioControladorTest.php
+ File description: testing function of class UsuarioControlador.php
+ Authors: Caique Pereira, Jessica Suzuki, João Gabriel, Macário Soares, Victor Cunha.
+*/ 
 
 require_once "../../Modelo/Usuario.php";
 require_once "../../Controle/UsuarioControlador.php";
