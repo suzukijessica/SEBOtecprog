ce7a467ab44beda23f67b9d36a532a1ebebe348f
Yeltsinn <yeltsin.lobato@hotmail.com>
2014-03-28 01:13:03 -0300

    Header excluiUsuario

--- SeboEletronicov2.0/Visao/excluiUsuario.php
+++ SeboEletronicov2.0/Visao/excluiUsuario.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ File name: excluiUsuarui.php
+ File description: data view to delete the user
+ Authors: Caique Pereira, Jessica Suzuki, João Gabriel, Macário Soares, Victor Cunha.
+*/
+
 session_start();
 $id_usuario = $_SESSION['id_usuario'];
 $senha = $_SESSION['senha'];
