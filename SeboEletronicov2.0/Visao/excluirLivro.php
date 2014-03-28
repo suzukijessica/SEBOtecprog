92bd55a492f0d5afd05ef662292c4468542d9a53
Yeltsinn <yeltsin.lobato@hotmail.com>
2014-03-28 01:14:17 -0300

    Header excluirLivro

--- SeboEletronicov2.0/Visao/excluirLivro.php
+++ SeboEletronicov2.0/Visao/excluirLivro.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ File name: excluirLivro.php
+ File description: data view to delete the Livro
+ Authors: Caique Pereira, Jessica Suzuki, João Gabriel, Macário Soares, Victor Cunha.
+*/
+
 session_start();
 $id_usuario = $_SESSION['id_usuario'];
 ?>
