b6f84c3363945ad6a2c0f87c4f013330a562e80c
suzukijessica <suzuki_jessica@hotmail.com>
2014-05-02 10:32:32 -0300

    Changing language of Utilities classes

--- SeboEletronicov2.0/Utilities/ExceptionPublishingWrong.php
+++ SeboEletronicov2.0/Utilities/ExceptionPublishingWrong.php
@@ -3,7 +3,6 @@
 /*
   File name: ExcessaoEditora.php
   File description: establishes exception when an invalid editor is inserted.
-  Authors: Caique Pereira, Jessica Suzuki, João Gabriel, Macário Soares, Victor Cunha
  */
 
 class ExcessaoEditoraInvalida extends Exception {
