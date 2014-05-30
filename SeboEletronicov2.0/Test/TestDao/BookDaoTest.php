a134b89d7c65f64c4f338e4415bc9d9dd5b8bf00
Priscilla Gonçalves da Silva e Souza <priscilla_hasya@windowslive.com>
2014-05-30 11:10:09 -0300

    comentaries of functions parameters and returns in class TestDao

--- SeboEletronicov2.0/Test/TestDao/BookDaoTest.php
+++ SeboEletronicov2.0/Test/TestDao/BookDaoTest.php
@@ -1,7 +1,7 @@
 <?php
 /*
  File name: BookDaoTest.php
- File description: testing functions of class LivroDaoTest.php.
+ File description: testing functions of class BookDao.php.
 */ 
 
 require_once "../../Model/BookModel.php";
@@ -18,6 +18,11 @@ class BookDaoTest extends PHPUnit_Framework_TestCase {
     protected $book;
 
     protected function setUp() {
+        /**
+         * creates objects which will be used in the tests
+         * no parameters
+         * no return
+        */
         $this->book = new BookModel('calculo 1', 'Thomas', 'engenharia', 2, 'editora teste', 'venda', 'troca', 'novo', 'livro e muito legal');
         $this->bookDaoTest = new BookDao();
     }
@@ -25,71 +30,129 @@ class BookDaoTest extends PHPUnit_Framework_TestCase {
     protected function tearDown() {
         unset($this->bookDaoTest);
         unset($this->book);
-        
+        /**
+         * clean objects which were used in the tests
+         * no parameters
+         * no return
+        */
     }
 
     public function testSaveBook() {
+         /**
+         * tests save a book in database with all its parameters
+         * no parameters
+         * no return
+        */
         $return = $this->bookDaoTest->savesBookDao($this->book, 23);
         $this->assertTrue($return);
     }
     
      public function testSearchBook() {
+         /**
+         * tests search for a book in database with all its parameters
+         * no parameters
+         * no return
+        */
         $return = $this->bookDaoTest->searchesBookDao($this->book->getTitulo(), 'novo', 'usado', 
                 'venda', 'troca');
         $this->assertFalse($return);
     }
 
     public function testGetBookByIdDaoWithNullId() {
+         /**
+         * tests get a book in database using identifier null
+         * no parameters
+         * no return
+        */
         $return = $this->bookDaoTest->getBookByIdDao(null);
         $this->assertFalse($return);    
     }
     
     public function testGetBookByIdDaoWithNegativeId() {
+        /**
+         * tests get a book in database using negative identifier
+         * no parameters
+         * no return
+        */
         $return = $this->bookDaoTest->getBookByIdDao(-3);
         $this->assertFalse($return);    
     }
     
     public function testGetBookByIdDaoWithValidId() {
-            $return = $this->bookDaoTest->getBookByIdDao(7);
-            $this->assertEquals(23, $return[1]);
-            $this->assertEquals($this->book->getTitle(), $return[2]);
-            $this->assertEquals($this->book->getPublisher(), $return[3]);
-            $this->assertEquals($this->book->getAuthor(), $return[4]);
-            $this->assertEquals($this->book->getEdition(), $return[5]);
-            $this->assertEquals($this->book->getGenre(), $return[6]);
-            $this->assertEquals($this->book->getStatus(), $return[7]);
-            $this->assertEquals($this->book->getDescription(), $return[8]);
-            $this->assertEquals($this->book->getSale(), $return[9]);
-            $this->assertEquals($this->book->getSwap(), $return[10]);  
+        /**
+         * tests get a book in database using valid identifier
+         * no parameters
+         * no return
+        */
+         $return = $this->bookDaoTest->getBookByIdDao(7);
+         $this->assertEquals(23, $return[1]);
+         $this->assertEquals($this->book->getTitle(), $return[2]);
+         $this->assertEquals($this->book->getPublisher(), $return[3]);
+         $this->assertEquals($this->book->getAuthor(), $return[4]);
+         $this->assertEquals($this->book->getEdition(), $return[5]);
+         $this->assertEquals($this->book->getGenre(), $return[6]);
+         $this->assertEquals($this->book->getStatus(), $return[7]);
+         $this->assertEquals($this->book->getDescription(), $return[8]);
+         $this->assertEquals($this->book->getSale(), $return[9]);
+         $this->assertEquals($this->book->getSwap(), $return[10]);  
     }
-
     public function testDeleteBook() {
+        /**
+         * tests delete a book from database
+         * no parameters
+         * no return
+        */
         $return = $this->bookDaoTest->deletesBookDao(15);
         $this->assertTrue($return);    
     }
 
 
     public function testChangeBook() {
+         /**
+         * tests change informations about a book in database
+         * no parameters
+         * no return
+        */
         $return = $this->bookDaoTest->changesBookDao($this->book, 23,16);
         $this->assertTrue($return);    
     }
 
     public function testGetBookByIdUser() {
+         /**
+         * tests get a book in database using user identifier
+         * no parameters
+         * no return
+        */
         $return = $this->bookDaoTest->getBookByIdUserDao(23);
         $this->assertFalse($return);    
     }
     
     public function testGetBookByIdUserWithValidReturn() {
+        /**
+         * tests get a book in database using user identifier valid return of function
+         * no parameters
+         * no return
+        */
         $return = $this->bookDaoTest->getBookByIdUserDao(23);
         $this->assertNotSame(null, $return);    
     }
     
     public function testGetAllBook(){
+        /**
+         * tests get all books in database
+         * no parameters
+         * no return
+        */
         $return = $this->bookDaoTest->getAllBookDao(23);
         $this->assertFalse($return);
     }
     
     public function testGetAllBookWithValidReturn(){
+        /**
+         * tests get all books in database with valid return
+         * no parameters
+         * no return
+        */
         $return = $this->bookDaoTest->getAllBookDao(23);
         $this->assertNotSame(null, $return);
     }
--- SeboEletronicov2.0/Test/TestDao/UserDaoTest.php
+++ SeboEletronicov2.0/Test/TestDao/UserDaoTest.php
@@ -19,48 +19,93 @@ class UserDaoTest extends PHPUnit_Framework_TestCase {
     protected $user;
     
     protected function setUp() {
+        /**
+         * creates objects which will be used in the tests
+         * no parameters
+         * no return
+        */
         $password = array(123123,123123);
         $this->user = new UserModel('lucas', 98989898, 'lucas@lucas.com', $password);
         $this->userDaoTest = new UserDao();
     }
 
     protected function tearDown() {
+        /**
+         * clean objects which were used in the tests
+         * no parameters
+         * no return
+        */
         unset($this->userDaoTest);
         unset($this->user);
     }
 
     public function testSaveUser() {
+         /**
+         * tests save a user in database with all its parameters
+         * no parameters
+         * no return
+        */
         $return = $this->userDaoTest->savesUserDao($this->user);
         $this->assertTrue($return);
     }
     
     public function testChangeUser(){
+         /**
+         * testschange informations about a user in database with all its parameters
+         * no parameters
+         * no return
+        */
         $return = $this->userDaoTest->changesUserDao($this->user, 11, 123456);
         $this->assertTrue($return);
     }
     
     public function testSearchUser(){
+         /**
+         * tests search a user in database with all its parameters
+         * no parameters
+         * no return
+        */
         $return = $this->userDaoTest->searchesUserDao(null);
         $this->assertFalse($return);
     }
     
     public function testDeleteUser(){
+         /**
+         * tests delete a user from database
+         * no parameters
+         * no return
+        */
         $password = $this->user->getPassword();
         $return = $this->userDaoTest->deletesUserDao($this->user->getEmail(), $password[0]);
         $this->assertTrue($return);
     }
     
     public function testGetCadastredByIdWithNullId(){
+         /**
+         * tests get user cadastre with identifier null
+         * no parameters
+         * no return
+        */
         $return = $this->userDaoTest->getCadastredById(null);
         $this->assertFalse($return);
     }
     
     public function testGetCadastredByIdWithInvalidId(){
+         /**
+         *  tests get user cadastre with invalid identifier 
+         * no parameters
+         * no return
+        */
         $return = $this->userDaoTest->getCadastredById(-2);
         $this->assertFalse($return);
     }
     
     public function testGetCadastredByIdWithValideId(){
+        /**
+         *  tests get user cadastre with valid identifier 
+         * no parameters
+         * no return
+        */
         $password = $this->user->getPassword();
         $return = $this->userDaoTest->getCadastredById(23);
         $this->assertEquals($this->user->getEmail(), $return[4]);
